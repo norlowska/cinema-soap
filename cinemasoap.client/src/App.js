8ea5195c8180bc106ab0cf176c8039b0d200f2b3
Natalia Orłowska <mcb.nat@gmail.com>
2021-05-05 10:17:45 +0200

    Create client project

---
+++ cinemasoap.client/src/App.js
@@ -0,0 +1,8 @@
+import { Button } from "antd";
+import "./App.css";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
---
+++ cinemasoap.client/src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "antd/dist/antd.css";
+import { ConfigProvider } from "antd";
+import plPL from "antd/lib/locale/fr_FR";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ConfigProvider locale={plPL}>
+      <App />
+    </ConfigProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
